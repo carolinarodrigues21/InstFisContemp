07c0470bc7da64b1c60718a2049845da5beb2852
carolinarodrigues21 <carolina.nmrr@gmail.com>
2022-06-22 15:47:04 -0300

    Altera Volume com sensor ultrassonico

--- Theremin/alteraVolume.ino
+++ Theremin/alteraVolume.ino
@@ -1,22 +1,27 @@
 const int pinPot = A4; // Indica a porta analógica ligada ao potenciômetro
 const int pinBuzzer = 3; // Indica a porta digital ligada ao buzzer (PWM)
+#define trigPin 7 // Declara constante trigPin
+#define echoPin 8 // Declara constante como echoPin
+int duration, distance; // Inicializa variável de distância e tempo
 
 void setup(){
   pinMode(pinBuzzer, OUTPUT);
-  pinMode(pinPot, INPUT); // Por padrão porta analógica já é INPUT
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop(){
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1; // Define base do cálculo de conversão
   
-  int potValue = analogRead(pinPot); // Lê valores no potenciômetro
-  int pwm = map(potValue, 0, 1024, 0, 255); //define valores pwm (0 a 255)
-  float volt = potValue * (5.00/1023.0); //Valores da tensão no circuito
+  int pwm = map(distance, 0, 40, 0, 255); //define valores pwm (0 a 255)o
   
-  Serial.print("V = ");
-  Serial.print(potValue);
-  Serial.print(" | T = ");
-  Serial.print(volt);
+  Serial.print("D = ");
+  Serial.print(distance);
   Serial.print(" | PWM = ");
   Serial.println(pwm);
 
