669be4b6fe97c9fb464fc2e8ce4a91566bef5c04
Carolina Rodrigues <70293019+carolinarodrigues21@users.noreply.github.com>
2022-07-01 14:55:31 -0300

    Speaker volume and frequency control

--- Theremin/theremino.ino
+++ Theremin/theremino.ino
@@ -1,4 +1,10 @@
+#include "Volume.h"
 
+Volume vol;
+
+// Speaker pin 5
+
+int Speaker = 5;
 
 // Declara trig e echo do primeiro sensor (Frequência)
 #define trigPin1 4 // Declara constante trigPin
@@ -8,10 +14,16 @@
 #define trigPin2 7 // Declara constante trigPin
 #define echoPin2 8 // Declara constante como echoPin
 
-int frequencia = 0; // Inicializa variável de frequência em 0
-int Buzzer = 3; // Inicializa buzzer no pino 13
+ // Inicializa variável de frequência em 0
 int duration1, distance1 ; // Inicializa variável de distância e tempo
 int duration2, distance2 ; // Inicializa variável de distância e tempo
+
+int frequencia = 0;
+
+int freq1 = 10;
+int freq2 = 50;
+int freq3 = 150;
+int freq4 = 250;
 
 void setup() {
  Serial.begin (9600);
@@ -20,8 +32,11 @@ void setup() {
  
  pinMode(trigPin2, OUTPUT);
  pinMode(echoPin2, INPUT);
+
+ pinMode(Speaker,OUTPUT);
+
+ vol.begin();
  
- pinMode(Buzzer, OUTPUT);
 }
 
 void loop() {
@@ -32,34 +47,39 @@ void loop() {
  distance1 = (duration1 / 2) / 29.1; // Define base do cálculo de conversão
  
  if (distance1 >= 40 || distance1 <= 0) { // Define as distâncias bases de verificação
+  frequencia = 0;
   Serial.print(" | D1 = ");
   Serial.print(distance1);
   Serial.print(" range 5 de frequencia");
-  noTone(Buzzer);
+  noTone(Speaker);
  }
  else if (distance1>= 30 && distance1<= 40 ) { // Define as distâncias bases de verificação
+  frequencia = freq4;
   Serial.print(" | D1 = ");
   Serial.print(distance1);
   Serial.print(" range 4 de frequencia");
-  tone(Buzzer, 150);
+  tone(Speaker, freq4);
  }
  else if (distance1>= 20 && distance1<= 30 ) { // Define as distâncias bases de verificação
+  frequencia = freq3;
   Serial.print(" | D1 = ");
   Serial.print(distance1);
   Serial.print(" range 3 de frequencia");
-  tone(Buzzer, 100);
+  tone(Speaker, freq3);
  }
  else if (distance1>= 10 && distance1<= 20 ) { // Define as distâncias bases de verificação
+  frequencia = freq2;
   Serial.print(" | D1 = ");
   Serial.print(distance1);
   Serial.print(" range 2 de frequencia");
-  tone(Buzzer, 50);
+  tone(Speaker, freq2);
  }
  else { // Define as distâncias bases de verificação
+  frequencia = freq1;
   Serial.print(" | D1 = ");
   Serial.print(distance1);
   Serial.print(" range 1 de frequencia");
-  tone(Buzzer, 10);
+  tone(Speaker, freq1);
  }
 
  delay(100);
@@ -72,14 +92,14 @@ void loop() {
  distance2 = (duration2 / 2) / 29.1;
   
  //Controle de Volume
- int pwm = map(distance2, 0, 40, 0, 255); //define valores pwm (0 a 255)o
- Serial.print(" D2 = ");
+ byte volumes[4] = {255, 127, 12, 0};   // List of volumes: 100% Volume, 50% Volume, 5% Volume, 0% Volume
+ int mapeamento = map(distance2, 0, 40, volumes[3], volumes[0]); //define valores pwm (0 a 255)o
+  
+ Serial.print("D = ");
  Serial.print(distance2);
- Serial.print(" | PWM = ");
- Serial.println(pwm);
-
- analogWrite(Buzzer, pwm);
-
- delay(300);
+ Serial.print(" | MAP = ");
+ Serial.println(mapeamento);
+ vol.tone(frequencia, mapeamento);
+ delay(100);
  
 }
