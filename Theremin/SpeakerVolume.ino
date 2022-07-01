61c22d06217bcd8a46843dd0dc3bba5875d48456
Carolina Rodrigues <70293019+carolinarodrigues21@users.noreply.github.com>
2022-07-01 14:43:54 -0300

    Control Speaker Volume with Ultrasonic

--- Theremin/SpeakerVolume.ino
+++ Theremin/SpeakerVolume.ino
@@ -1,4 +1,7 @@
 #include "Volume.h" // Include the Volume library
+#define trigPin 7 // Declara constante trigPin
+#define echoPin 8 // Declara constante como echoPin
+int duration, distance; // Inicializa variável de distância e tempo
 
 // speaker connected to digital pin 5
 
@@ -7,11 +10,26 @@ Volume vol; // Plug your speaker into the default pin for your board type:
 
 void setup() {
   vol.begin();
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
-void loop() {
+ 
+void loop(){
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1; // Define base do cálculo de conversão
+
   byte volumes[4] = {255, 127, 12, 0};   // List of volumes: 100% Volume, 50% Volume, 5% Volume, 0% Volume
-  for (int i = 0; i < 4; i++) { // Iterate through volume list one second at a time
-    vol.tone(440, volumes[i]);
-    vol.delay(1000);
-  }
+  int mapeamento = map(distance, 0, 40, volumes[3], volumes[0]); //define valores pwm (0 a 255)o
+  
+  Serial.print("D = ");
+  Serial.print(distance);
+  Serial.print(" | MAP = ");
+  Serial.println(mapeamento);
+  vol.tone(440, mapeamento);
+  delay(100);
+     
 }
