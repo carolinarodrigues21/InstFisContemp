a48e18c3260fcb6e6dec08e53427a5be8ecc8523
Carolina Rodrigues <70293019+carolinarodrigues21@users.noreply.github.com>
2022-07-18 10:43:05 -0300

    Final Update
    Controle de Volume e frequencia simultânea
    LEDs indicativos da frequência

--- Theremin/theremino.ino
+++ Theremin/theremino.ino
@@ -18,12 +18,18 @@ int Speaker = 5;
 int duration1, distance1 ; // Inicializa variável de distância e tempo
 int duration2, distance2 ; // Inicializa variável de distância e tempo
 
+
 int frequencia = 0;
 
 int freq1 = 10;
 int freq2 = 50;
 int freq3 = 150;
 int freq4 = 250;
+
+int LED1 = 13;
+int LED2 = 12;
+int LED3 = 11;
+int LED4 = 3;
 
 void setup() {
  Serial.begin (9600);
@@ -35,62 +41,22 @@ void setup() {
 
  pinMode(Speaker,OUTPUT);
 
+ pinMode(LED1,OUTPUT);
+ pinMode(LED2,OUTPUT);
+ pinMode(LED3,OUTPUT);
+ pinMode(LED4,OUTPUT);
+
  vol.begin();
  
 }
 
-void loop() {
- digitalWrite(trigPin1, HIGH);
- delayMicroseconds(10);
- digitalWrite(trigPin1, LOW);
- duration1 = pulseIn(echoPin1, HIGH);
- distance1 = (duration1 / 2) / 29.1; // Define base do cálculo de conversão
- 
- if (distance1 >= 40 || distance1 <= 0) { // Define as distâncias bases de verificação
-  frequencia = 0;
-  Serial.print(" | D1 = ");
-  Serial.print(distance1);
-  Serial.print(" range 5 de frequencia");
-  noTone(Speaker);
- }
- else if (distance1>= 30 && distance1<= 40 ) { // Define as distâncias bases de verificação
-  frequencia = freq4;
-  Serial.print(" | D1 = ");
-  Serial.print(distance1);
-  Serial.print(" range 4 de frequencia");
-  tone(Speaker, freq4);
- }
- else if (distance1>= 20 && distance1<= 30 ) { // Define as distâncias bases de verificação
-  frequencia = freq3;
-  Serial.print(" | D1 = ");
-  Serial.print(distance1);
-  Serial.print(" range 3 de frequencia");
-  tone(Speaker, freq3);
- }
- else if (distance1>= 10 && distance1<= 20 ) { // Define as distâncias bases de verificação
-  frequencia = freq2;
-  Serial.print(" | D1 = ");
-  Serial.print(distance1);
-  Serial.print(" range 2 de frequencia");
-  tone(Speaker, freq2);
- }
- else { // Define as distâncias bases de verificação
-  frequencia = freq1;
-  Serial.print(" | D1 = ");
-  Serial.print(distance1);
-  Serial.print(" range 1 de frequencia");
-  tone(Speaker, freq1);
- }
-
- delay(100);
-
+void controleVolume(int frequencia){
  digitalWrite(trigPin2, HIGH);
  delayMicroseconds(10);
  digitalWrite(trigPin2, LOW);
  duration2 = pulseIn(echoPin2, HIGH);
 
  distance2 = (duration2 / 2) / 29.1;
-  
  //Controle de Volume
  byte volumes[4] = {255, 127, 12, 0};   // List of volumes: 100% Volume, 50% Volume, 5% Volume, 0% Volume
  int mapeamento = map(distance2, 0, 40, volumes[3], volumes[0]); //define valores pwm (0 a 255)o
@@ -100,6 +66,77 @@ void loop() {
  Serial.print(" | MAP = ");
  Serial.println(mapeamento);
  vol.tone(frequencia, mapeamento);
+ delay(100);
+}
+
+void loop() {
+ digitalWrite(trigPin1, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigPin1, LOW);
+ duration1 = pulseIn(echoPin1, HIGH);
+ distance1 = (duration1 / 2) / 29.1; // Define base do cálculo de conversão
+ 
+ if (distance1 >= 25 || distance1 <= 0) { // Define as distâncias bases de verificação
+  frequencia = 0;
+  Serial.print(" | D1 = ");
+  Serial.print(distance1);
+  Serial.print(" range 5 de frequencia \n");
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED4,LOW);
+  controleVolume(0);
+  //noTone(Speaker);
+ }
+ else if (distance1>= 20 && distance1<= 25 ) { // Define as distâncias bases de verificação
+  frequencia = freq4;
+  Serial.print(" | D1 = ");
+  Serial.print(distance1);
+  Serial.print(" range 4 de frequencia");
+  digitalWrite(LED4,HIGH);
+  digitalWrite(LED3,HIGH);
+  digitalWrite(LED2,HIGH);
+  digitalWrite(LED1,HIGH);
+  controleVolume(frequencia);
+  // tone(Speaker, freq4);
+ }
+ else if (distance1>= 15 && distance1<= 20) { // Define as distâncias bases de verificação
+  frequencia = freq3;
+  digitalWrite(LED4,LOW);
+  digitalWrite(LED3,HIGH);
+  digitalWrite(LED2,HIGH);
+  digitalWrite(LED1,HIGH);
+  Serial.print(" | D1 = ");
+  Serial.print(distance1);
+  Serial.print(" range 3 de frequencia \n");
+  controleVolume(frequencia);
+  // tone(Speaker, freq3);
+ }
+ else if (distance1>= 10 && distance1<= 15 ) { // Define as distâncias bases de verificação
+  frequencia = freq2;
+  digitalWrite(LED4,LOW);
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED2,HIGH);
+  digitalWrite(LED1,HIGH);
+  Serial.print(" | D1 = ");
+  Serial.print(distance1);
+  Serial.print(" range 2 de frequencia \n");
+  controleVolume(frequencia);
+  // tone(Speaker, freq2);
+ }
+ else { // Define as distâncias bases de verificação
+  frequencia = freq1;
+  digitalWrite(LED1,HIGH);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED4,LOW);
+  Serial.print(" | D1 = ");
+  Serial.print(distance1);
+  Serial.print(" range 1 de frequencia \n");
+  controleVolume(frequencia);
+  // tone(Speaker, freq1);
+ }
+
  delay(100);
  
 }
